646fc0dc9cf97264af249f2cf4354c561c7e04bd
Gordon Zhao <gordonzhao7@gmail.com>
2023-01-21 21:31:41 -0800

    added store for redux-toolkit

---
+++ src/store/country-slice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const countrySlice = createSlice({
+	name: 'country',
+	initialState: {
+		loading: false,
+		error: false,
+		countryInput: '',
+		countryData: {
+			confirmCases: null,
+			activeCases: null,
+			deathCases: null,
+			recoveredCases: null,
+		},
+	},
+	reducers: {
+		setCountryInput(state, action) {
+			state.countryInput = action.payload;
+		},
+		setCountryData(state, action) {
+			const data = action.payload;
+			countryData = Object.assign(countryData, data);
+		},
+	},
+});
+
+export default cartSlice;
